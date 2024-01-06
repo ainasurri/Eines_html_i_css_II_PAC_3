cde0a47f871d3bbff02d4be2af4fa18b4cbc84af
AinaSorribas <122831540+AinaSorribas@users.noreply.github.com>
2024-01-06 19:05:52 +0100

    article finish w/issues footer & nav

---
+++ uoc-boilerplate-master/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html", "./src/participants.html"],
+  theme: {
+    extend: {
+      colors: {
+        'gris': '#f7f7f7',
+        'blau': '#02055D',
+        'salmo': '#FF6752',
+        'grisfosc': '#D3D3D3'
+      },
+      fontFamily: {
+        'lucida': ['Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 'Geneva', 'Verdana', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
